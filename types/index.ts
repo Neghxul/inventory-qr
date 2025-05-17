da731461155f644329e6a7642efb46fd8fcb0532
Neghxul <cgrr29us@gmail.com>
2025-05-17 11:36:06 -0600

    chore: regenerate Prisma Client during build

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,25 @@
+// src/types.ts
+
 export interface ScanData {
   id: number;
-  sessionId: string;
-  code?: string;
-  clave: string;
+  sessionId?: string;
+
+  key: string;
+  year: string;
   pedimento: string;
-  descripcion?: string;
-  linea?: string;
-  estante?: string;
-  posicion?: string;
-  cantidad?: number;
-  codificado?: string;
-  tipo?: string;
+
+  description?: string;
+  line?: string;
+  shelf?: string;
+  position?: string;
+
+  quantity?: number;
+  stock?: number;
+
+  // <<── Añade estas dos
+  encoded?: boolean;
+  type?:    string;
+  // ──────────────────>>
+
   createdAt?: string;
 }
